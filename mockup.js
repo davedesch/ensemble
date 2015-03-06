22f80bed63ccbf7379ec2732f374b97013825498
Dave & Saleh Rastani <pair+davedesch.salehrastani@devbootcamp.com>
2015-03-06 11:19:19 -0800

    get ajax call to pickup url for new single outfit

--- mockup.js
+++ mockup.js
@@ -14,7 +14,7 @@ function addNewOutfit(){
     url: "http://www.reddit.com/r/aww/comments/2y3fas/look_at_this_pile_of_30_dogs_posing_and_looking/.json"
   }).done(function(data){
     // console.log(data);
-    // console.log(data[0].data.children.data)
+    console.log(data[0].data.children[0].data.url)
   })
 }
 
