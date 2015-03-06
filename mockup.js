33745758f84dddf94a26a8a24b3f0d946d776f21
Dave & Saleh Rastani <pair+davedesch.salehrastani@devbootcamp.com>
2015-03-06 11:18:12 -0800

    add partially working ajax call for new outfit template

---
+++ mockup.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  console.log('ready!')
+  addNewOutfit();
+
+
+})
+
+function addNewOutfit(){
+  var source = $("#single-outfit-template").html();
+  var template =Handlebars.compile(source);
+  var context = {}
+
+  $.ajax({
+    url: "http://www.reddit.com/r/aww/comments/2y3fas/look_at_this_pile_of_30_dogs_posing_and_looking/.json"
+  }).done(function(data){
+    // console.log(data);
+    // console.log(data[0].data.children.data)
+  })
+}
+
+
+
+
+
+
