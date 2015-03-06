94069a795400b91c4251a9fea8702a5925f69502
Dave & Saleh Rastani <pair+davedesch.salehrastani@devbootcamp.com>
2015-03-06 11:30:48 -0800

    ajax is working on sample json, populating feed correctly

--- mockup.js
+++ mockup.js
@@ -14,7 +14,8 @@ function addNewOutfit(){
     url: "http://www.reddit.com/r/aww/comments/2y3fas/look_at_this_pile_of_30_dogs_posing_and_looking/.json"
   }).done(function(data){
     // console.log(data);
-    console.log(data[0].data.children[0].data.url)
+    context = {outfit: data[0].data.children[0].data}
+    $('#single-outfit').prepend(template(context))
   })
 }
 
