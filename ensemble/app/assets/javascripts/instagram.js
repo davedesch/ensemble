ca94e142aaa70187788ad890a25f93d1aa0d047d
immber <jessiekrushing@hotmail.com>
2015-03-06 15:54:35 -0800

    starts to add ajax and setup functions in instagram controller

--- ensemble/app/assets/javascripts/instagram.js
+++ ensemble/app/assets/javascripts/instagram.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
     console.log('working')
+    $('#instagram').on('click', function(){
+    	console.log('clicked')
+    	CLIENT-ID=
+    	REDIRECT=
+    	code
+
+    	$.ajax({
+    		url: '/instagram/new'
+    		type: 'Get',
+    		dataType: 'html',
+    	})
+    	.done(function() {
+    		console.log("success");
+    	})
+    	.fail(function() {
+    		console.log("error");
+    	})
+    	.always(function() {
+    		console.log("complete");
+    	});
+    	
+    })
 });
--- ensemble/app/controllers/instagrams_controller.rb
+++ ensemble/app/controllers/instagrams_controller.rb
@@ -2,6 +2,12 @@ class InstagramsController < ApplicationController
 
 	def index
 	end
-	
+
+	def login
+		CLIENT-ID=
+    	REDIRECT=
+    	code= 
+    end
+
 
 end
