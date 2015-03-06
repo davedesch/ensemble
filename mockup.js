9fd35d583efc0d18cc2144f5aeec2edfb97ad338
Dave & Saleh Rastani <pair+davedesch.salehrastani@devbootcamp.com>
2015-03-06 14:12:58 -0800

    comment out addnewoutfit ajax call

--- mockup.js
+++ mockup.js
@@ -1,54 +1,67 @@
 $(document).ready(function(){
 
-  addNewOutfit();
+  displayAllOutfits();
   displayRecentOutfits();
   displayTrendingHashtags();
 
 })
 
 
-function addNewOutfit(){
-  var source = $("#single-outfit-template").html();
+function displayAllOutfits(){
+  var source = $("#all-outfits-template").html();
   var template =Handlebars.compile(source);
   var context = {}
 
   $.ajax({
-    url: "http://www.reddit.com/r/aww/comments/2y3fas/look_at_this_pile_of_30_dogs_posing_and_looking/.json"
+    url: "http://www.reddit.com/r/aww/top/.json"
   }).done(function(data){
-    context = {outfit: data[0].data.children[0].data}
-    $('#single-outfit').prepend(template(context))
+    context = {allOutfits: data.data.children};
+    $('#all-outfits').html(template(context))
   })
 }
+
+// just in case we want to prepend a newly created outfit...
+
+// function addNewOutfit(){
+//   var source = $("#new-outfit-template").html();
+//   var template =Handlebars.compile(source);
+//   var context = {}
+
+//   $.ajax({
+//     url: "http://www.reddit.com/r/aww/comments/2y3fas/look_at_this_pile_of_30_dogs_posing_and_looking/.json"
+//   }).done(function(data){
+//     context = {outfit: data[0].data.children[0].data}
+//     $('#all-outfits').prepend(template(context))
+//   })
+// }
 
 function displayRecentOutfits (){
   var source = $("#recent-outfits-template").html();
   var template =Handlebars.compile(source);
-  var context = {recentOutfits: []};
+  var context = {};
 
   $.ajax({
     url: "http://www.reddit.com/r/aww/top/.json"
   }).done(function(data){
-    context.recentOutfits = data.data.children;
-    // console.log(context.recentOutfits)
-    // context = {outfits: data[0].data.children[0].data}
+    context = {recentOutfits: data.data.children};
     $('#recent-outfits').html(template(context));
   })
 }
 
 function displayTrendingHashtags (){
   var source = $("#trending-hashtags-template").html();
-  var template =Handlebars.compile(source);
-  var context = {trendingHashtags: []};
+  var template = Handlebars.compile(source);
+  var context = {};
 
   $.ajax({
     url: "http://www.reddit.com/r/aww/top/.json"
   }).done(function(data){
-    context.trendingHashtags = data.data.children;
-    // console.log(context.trendingHashtags)
-    // context = {outfits: data[0].data.children[0].data}
+    context = {trendingHashtags: data.data.children};
     $('#trending-hashtags').html(template(context));
   })
 }
+
+
 
 
 
