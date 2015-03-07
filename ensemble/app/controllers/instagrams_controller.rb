debc160ace79b1f08c2585eeda0ea109b0cd2476
immber <jessiekrushing@hotmail.com>
2015-03-06 20:11:16 -0800

    instagram oauth step one working

--- ensemble/app/controllers/instagrams_controller.rb
+++ ensemble/app/controllers/instagrams_controller.rb
@@ -1,22 +1,23 @@
 class InstagramsController < ApplicationController
-  CALLBACK_URL = "http://localhost:3000/oauth/callback"
 
-  Instagram.configure do |config|
-    config.client_id = ENV['CLIENT_ID']
-    config.client_secret = ENV['CLIENT_SECRET']
-  end
-
+  CALLBACK_URL = "http://localhost:3000/instagram/oauth/callback"
+  
+  CLIENT = ENV['CLIENT_ID']
 
   def index
   end
 
+  #handles 'oauth/connect'
   def login
-    redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+    # render :nothing => true, :status => 200, :content_type => 'text/html'
+    redirect_to "https://api.instagram.com/oauth/authorize/?client_id=#{CLIENT}&redirect_uri=#{CALLBACK_URL}&response_type=code"
+    
   end
 
+  #handles 'oauth/callback'
   def authorized
     response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
-    session[:access_token] = response.access_token
+    
     redirect "/nav"
   end
 
