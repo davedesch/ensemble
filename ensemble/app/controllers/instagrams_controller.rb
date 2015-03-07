b9342d7c5df0829c7432a7ce467cdc23461d3323
immber <jessiekrushing@hotmail.com>
2015-03-06 23:12:11 -0800

    adds first or create user method for logging in with instagram, doesn't work, not returning a user

--- ensemble/app/controllers/instagrams_controller.rb
+++ ensemble/app/controllers/instagrams_controller.rb
@@ -18,23 +18,26 @@ class InstagramsController < ApplicationController
   def authorized
     #get token from teh server
     response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
-    create_or_login(response)
+    user = create_or_login(response)
     
     # user = User.find_or_create_by(username: response.user.username, avatar: response.user.profile_picture, auth_token: response.access_token, instagram_name: response.user.full_name)
 
-    # session[:user_id] = user.id
+    session[:user_id] = user.id
 
-    # redirect_to user_path(user.id)
+    redirect_to user_path(user.id)
   end
 
 private
 
   def create_or_login(response)
-    p response
-    p response.user.username
-    p response.user.profile_picture
-    p response.access_token
-    p response.user.full_name
+   user = User.where(username: response.user.username).first_or_create do |user|
+        username = response.user.username
+        avatar = response.user.profile_picture
+        auth_token = response.access_token
+        instagram_name = response.user.full_name
+      end
+    return user
   end
+
 
 end
