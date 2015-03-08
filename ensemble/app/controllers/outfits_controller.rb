8eb2a60223e05d4d49583b276c1676bea16c4dc2
p3t3rfp & Saleh Rastani <pair+p3t3rfp.salehrastani@devbootcamp.com>
2015-03-07 21:35:07 -0800

    outfits controller new and create actions are done yay

--- ensemble/app/controllers/outfits_controller.rb
+++ ensemble/app/controllers/outfits_controller.rb
@@ -11,11 +11,19 @@ class OutfitsController < ApplicationController
   end
 
   def new
-    # render new upload form
+    render html: :new
   end
 
   def create
-    #recieve params for new outfit
+    user = User.find(session[:user_id])
+    Outfit.create(outfit_params)
+    redirect_to user_path(user)
+  end
+
+  private
+
+  def outfit_params
+    params.require(:outfit).permit(:image_url, :user_id, :title, :catpion, :gender)
   end
 
 end
