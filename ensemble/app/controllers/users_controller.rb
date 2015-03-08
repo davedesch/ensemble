4613d0267249b5bc1720b16d45924bc539ee36a7
Dave Descheneaux <davedesch@gmail.com>
2015-03-07 16:15:37 -0800

    added create user method and test

--- ensemble/app/controllers/users_controller.rb
+++ ensemble/app/controllers/users_controller.rb
@@ -6,15 +6,19 @@ class UsersController < ApplicationController
   end
 
 
-  def new
-    # nothing: show/hide registration form
-  end
-
   def create
-    # recieve: username, email, password
+    p params
+    prospective_user = User.new(user_params)
+    if prospective_user.save
+      current_user = prospective_user
+      session[:user_id] = current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
-  def show
+
+
+def show
     #render dashboard
   end
 
@@ -42,9 +46,15 @@ class UsersController < ApplicationController
   end
 
 
-def give_token
+  def give_token
     session[:user_id] = @user.id
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :avatar)
+  end
+
 
 
 
--- ensemble/spec/controllers/users_controller_spec.rb
+++ ensemble/spec/controllers/users_controller_spec.rb
@@ -1,6 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe 'GET #index' do
+    it 'renders the :index template' do
+      get :index
+      expect(response).to render_template :index
+    end
+
+    it 'returns a 200 status' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+
+  # describe 'POST #create' do
+    # before :each do
+    #   @users = attributes_for(:user)
+    # end
+    context "with valid attributes" do
+      p FactoryGirl.attributes_for(:user)
+      it "creates new contact" do
+        expect{
+          post :create, user: FactoryGirl.attributes_for(:user)
+        }.to change(User, :count).by(1)
+      end
+    end
 
 
 end
