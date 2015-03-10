d80d16e5bbe9073b3cd2125509dacc23c62494df
Dave Descheneaux <davedesch@gmail.com>
2015-03-10 14:52:47 -0700

    fixed bad routes in users controller

--- ensemble/app/controllers/users_controller.rb
+++ ensemble/app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user.id)
     else
       @errors = prospective_user.errors
-      redirect_to :index
+      redirect_to '/'
     end
   end
 
