03cf0ca71dc7932a971645b8ef68f8f8cb45accd
p3t3rfp & Saleh Rastani <pair+p3t3rfp.salehrastani@devbootcamp.com>
2015-03-11 10:37:04 -0700

    articles are on outfit form, yay

--- ensemble/db/seeds.rb
+++ ensemble/db/seeds.rb
@@ -1,34 +1,38 @@
-10.times do
-  puts "making a user"
-  user = FactoryGirl.create :user
-  puts user.id
+# 10.times do
+#   puts "making a user"
+#   user = FactoryGirl.create :user
+#   puts user.id
   puts "giving them 3 outfits"
   3.times do
-    outfit = FactoryGirl.create :outfit, :with_hashtags, user: user
+    outfit = FactoryGirl.create :outfit, :with_hashtags, user: User.first
     puts outfit
     FactoryGirl.create :article, outfit: outfit
   end
-  puts "*" * 100
-end
+#   puts "*" * 100
+# end
 
-outfits = Outfit.all
-outfits.each do |outfit|
-  puts "for each outfit"
-  2.times do
-    puts "making 2 ratings"
-    n = User.count - 1
-    id = rand(1..n)
-    new_user = User.find(n)
-    outfit.ratings.create(user: new_user, comment: Faker::Hacker.say_something_smart, stars: 3)
-  end
-  2.times do
-    puts "making rating with stars only"
-    n = User.count - 1
-    id = rand(1..n)
-    new_user = User.find(n)
-    outfit.ratings.create(user: new_user, stars: 2)
-  end
-  puts "*" * 100
-end
+# outfits = Outfit.all
+# outfits.each do |outfit|
+#   puts "for each outfit"
+#   2.times do
+#     puts "making 2 ratings"
+#     n = User.count - 1
+#     id = rand(1..n)
+#     new_user = User.find(n)
+#     outfit.ratings.create(user: new_user, comment: Faker::Hacker.say_something_smart, stars: 3)
+#   end
+#   2.times do
+#     puts "making rating with stars only"
+#     n = User.count - 1
+#     id = rand(1..n)
+#     new_user = User.find(n)
+#     outfit.ratings.create(user: new_user, stars: 2)
+#   end
+#   puts "*" * 100
+# end
+
+ArticleType.create(type_desc: "Blouse")
+ArticleType.create(type_desc: "Necklace")
+ArticleType.create(type_desc: "Jeans")
 
 
